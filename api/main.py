edee6cb9f8d2774c10feb305805b4e42f12e71d6
Sagar Athani <sagar0418@example.com>
2025-03-30 15:13:22 +0530

    updated cors

--- api/main.py
+++ api/main.py
@@ -15,10 +15,10 @@ app = FastAPI()
 # Allow requests from the frontend running at localhost:3000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 @app.get("/")
